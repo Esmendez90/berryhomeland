38699762b5d7084500deec4262d9e40a6ae91813
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2024-11-08 11:46:03 -0500

    updated href paths

--- reactberryhomeland/src/Navigationbar.js
+++ reactberryhomeland/src/Navigationbar.js
@@ -13,8 +13,8 @@ function Navigationbar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="#/home">Home</Nav.Link>
+          <Nav.Link href="#/about">About</Nav.Link>
           {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
