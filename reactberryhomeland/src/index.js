0fdb52f1c89562a670d52db3d26c947a4fc5c12d
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2024-11-08 11:21:00 -0500

    using hashRouter

--- reactberryhomeland/src/index.js
+++ reactberryhomeland/src/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-    <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { HashRouter } from 'react-router-dom';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//     <App />
+//     </HashRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
