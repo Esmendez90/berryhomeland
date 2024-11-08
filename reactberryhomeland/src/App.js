5d8422c1b66ddfdb6a6961a77e4a5a08f2cd19d5
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2024-11-08 14:37:56 -0500

    added logo image to navigation bar

--- reactberryhomeland/src/App.js
+++ reactberryhomeland/src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { Routes, Route } from 'react-router-dom'; 
 import './App.css';
 import Navigationbar from './Navigationbar';
 import Home from "./Pages/Home";
--- reactberryhomeland/src/Navigationbar.js
+++ reactberryhomeland/src/Navigationbar.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Navigationbar() {
   return (
     <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#/home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#/home">Home</Nav.Link>
-          <Nav.Link href="#/about">About</Nav.Link>
-          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown> */}
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  </div>
-  )
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="#/home">
+            {" "}
+            <img
+              alt="logo"
+              src={process.env.PUBLIC_URL + "/images/logo.png"}
+              width="100%"
+              height="auto"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#/home">Home</Nav.Link>
+              <Nav.Link href="#/about">About</Nav.Link>
+              <Nav.Link href="#/about">Abaca Fiber Market</Nav.Link>
+              <Nav.Link href="#/about">Gallery</Nav.Link>
+              <Nav.Link href="#/about">Privacy Policy</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Navigationbar
+export default Navigationbar;
